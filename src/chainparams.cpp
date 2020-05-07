a06963f4389337e28d486b137e30a5891ff3982a
David Mah <david@wagerr.com>
2020-05-07 10:19:37 +0930

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -78,13 +78,14 @@ static const Checkpoints::CCheckpointData data = {
 static Checkpoints::MapCheckpoints mapCheckpointsTestnet =
     boost::assign::map_list_of
     (       0, uint256("00000fdc268f54ff1368703792dc046b1356e60914c2b5b6348032144bcb2de5"))
-    (       1, uint256("0000098cc93ece2804776d2e9eda2d01e2ff830d80bab22500821361259f8aa3"))     // 1567576895   2
-    (     450, uint256("3cec3911fdf321a22b8109ca95ca28913e6b51f0d80cc6d2b2e30e1f2a6115c0"))     // 1567615777   635
-    (     469, uint256("d69d843cd63d333cfa3ff4dc0675fa320d6ef8cab7ab1a73bf8a1482210f93ce"));    // 1588752598   662
+    (       1, uint256("0000098cc93ece2804776d2e9eda2d01e2ff830d80bab22500821361259f8aa3"))     
+    (     450, uint256("3cec3911fdf321a22b8109ca95ca28913e6b51f0d80cc6d2b2e30e1f2a6115c0"))     
+    (     469, uint256("d69d843cd63d333cfa3ff4dc0675fa320d6ef8cab7ab1a73bf8a1482210f93ce"))
+    (    1100, uint256("fa462709a1f3cf81d699ffbd45440204aa4d38de84c2da1fc8b3ff15c3c7a95f")); // 1588780440
 static const Checkpoints::CCheckpointData dataTestnet = {
     &mapCheckpointsTestnet,
-    1588752598,
-    662,
+    1588780440,
+    1924,
     1000};
 
 static Checkpoints::MapCheckpoints mapCheckpointsRegtest =
